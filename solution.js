252324016a8bf1f916835e4ae5a82e7b8e55181b
BorisDmitriev <113904873+BorisDmitriev@users.noreply.github.com>
2023-10-09 20:45:57 +0200

    Initial commit

---
+++ solution.js
@@ -0,0 +1,39 @@
+// write your code in this file
+
+// Q1
+console.log("Q1: " + (6450/15) + "tickets are sold");
+
+// Q2
+console.log("Q2: Dylva makes " + (52*500) + " $ every year");
+
+// Q3
+console.log("Q3: " + (17/30*100) + "%");
+
+// Q4
+console.log("Q4: " + (4.75*4) + " cm");
+
+// Q5
+console.log("Q5: " + (5+6+7) + " cm");
+
+// Q6
+console.log("Q6: " + (5**2) + " cm2");
+
+// Q7
+console.log("Q7: " + (1/2*3*4) + " cm2");
+
+// Q8
+console.log("Q8: " + (9*9*9) + " cm2");
+
+// Q9
+console.log("Q9: " + "€ " + (22.35*0.1+22.35) + " | " + " € " + (26.67*0.15+26.67) + " | " + "€ " + (35.92*0.2+35.92) );
+
+// Q10
+console.log("Q10: " + (8+6+5+9+8+2+1+8.5+7+4)/10 );
+
+// Q11
+console.log("Q11: Score in the sixth test: " + (85*6-(75+70+85+90+100)) );
+
+// Q12
+// (78 * 8 + y) / 9 = 80
+
+console.log("Q12: James nees a minimum of " + (80*9-8*78) + " % to get an 80% average");
